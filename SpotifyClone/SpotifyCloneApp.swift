88649aee4719e4020b24df72180e3d75218b4033
erenkara1907 <erenkaraaa47@gmail.com>
2022-11-25 12:47:44 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Eren Kara on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
